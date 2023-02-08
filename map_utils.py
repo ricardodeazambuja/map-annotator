1d14f7e86753cbfb7825db96d8607e2b2f0e68b9
ricardodeazambuja <ricardo.azambuja@gmail.com>
2023-02-08 18:11:41 -0500

    Added a limit and assert to play safe with tile requests

--- map_utils.py
+++ map_utils.py
@@ -5,18 +5,20 @@ import requests
 
 from PIL import Image
 
-import osmnx as ox # https://github.com/gboeing/osmnx
-import geopandas as gpd # https://geopandas.org/
-import mercantile as mc # https://github.com/mapbox/mercantile
+import osmnx as ox  # https://github.com/gboeing/osmnx
+import geopandas as gpd  # https://geopandas.org/
+import mercantile as mc  # https://github.com/mapbox/mercantile
 
 import rasterio.features
 import rasterio.transform
 
 BING_API_URL = "https://dev.virtualearth.net/REST/v1/Imagery/Metadata/{imagerySet}/{centerPoint}?zl={zoomLevel}&key={BING_KEY}"
 
+
 # https://social.msdn.microsoft.com/Forums/en-US/5454d549-5eeb-43a5-b188-63121d3f0cc1/how-to-set-zoomlevel-for-particular-altitude?forum=bingmaps
 def altitude2zoomlevel(altitudeMeters):
     return int(19 - math.log(altitudeMeters/1000 * 5.508, 2))
+
 
 # https://wiki.openstreetmap.org/wiki/Zoom_levels
 def h_dist(latitudeDegrees, altitudeMeters=None, zoomLevel=None):
@@ -35,7 +37,7 @@ def h_dist(latitudeDegrees, altitudeMeters=None, zoomLevel=None):
 # https://learn.microsoft.com/en-us/bingmaps/articles/bing-maps-tile-system
 def tile2quad(x, y, z):
     quad = ''
-    for i in range(z,0,-1):
+    for i in range(z, 0, -1):
         digit = 0
         mask = 1 << (i - 1)
         if ((x & mask) != 0):
@@ -64,7 +66,7 @@ def getAerialImage(lat, lon, zoomLevel, bing_key, bing_api_url=BING_API_URL):
 
 
 def getTiles(west, south, east, north, zoomLevel):
-    assert zoomLevel<=20, "zoomLevel>20!"
+    assert zoomLevel <= 20, "zoomLevel>20!"
     lst_tile = mc.bounding_tile(west, south, east, north)
     try:
         res = mc.children(lst_tile, zoom=zoomLevel)
@@ -76,24 +78,25 @@ def getTiles(west, south, east, north, zoomLevel):
 
 def getMask(west, south, east, north, imgSize, tags={"building": True}):
     west, south, east, north = mc.bounds(mc.bounding_tile(west, south, east, north))
-    maskShapely = gpd.clip(ox.geometries_from_bbox(north, south, east, west, tags), 
-            (west, south, east, north)).geometry.values
+    maskShapely = gpd.clip(ox.geometries_from_bbox(north, south, east, west, tags),
+                           (west, south, east, north)).geometry.values
     img = rasterio.features.rasterize(maskShapely, out_shape=imgSize, fill=0,
-                                  transform=rasterio.transform.from_bounds(west, south, east, north, *imgSize),
-                                  dtype='uint8')
+                                      transform=rasterio.transform.from_bounds(west, south, east, north, *imgSize),
+                                      dtype='uint8')
     return Image.fromarray(img*255)
 
 
-def getFusedImg(west, south, east, north, zoomLevel, bing_key):
+def getFusedImg(west, south, east, north, zoomLevel, bing_key, maxTiles=32):
     tiles = getTiles(west, south, east, north, zoomLevel)
+    assert len(tiles) <= maxTiles, f"You are trying to ask for more than {maxTiles} tiles, are you sure?"
     size = int(math.sqrt(len(tiles)))
     x,y,z = tiles[0]
-    totalSize = size*256
+    totalSize = size*256  # images (tiles) from Bing are 256x256
     finaImg = Image.new('RGB', (totalSize, totalSize))
     for i in range(size):
         for j in range(size):
-            tmpLngLat = mc.ul(mc.Tile(x+i,y+j,z))
+            tmpLngLat = mc.ul(mc.Tile(x+i, y+j, z))
             tmpImg = getAerialImage(tmpLngLat.lat, tmpLngLat.lng, z, bing_key)
-            finaImg.paste(tmpImg, (256*i,256*j))
-    
+            finaImg.paste(tmpImg, (256*i, 256*j))
+
     return finaImg
